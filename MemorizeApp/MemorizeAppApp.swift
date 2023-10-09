f32d529969fd17ceaf329a8f7ba04a8f8478fa08
vishelpodishat <alishsaideshov@gmail.com>
2023-10-09 13:35:41 +0600

    Initial Commit

---
+++ MemorizeApp/MemorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by Алишер Сайдешев on 09.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
